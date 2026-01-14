17f6e98ed511afdf60a874e4a0b71d35a117f474
Greener 2 <greener@MacBook-Air-de-Greener.local>
2026-01-14 11:12:55 +0100

    fix supabase key

--- public/Common/db.js
+++ public/Common/db.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "https://qzhzudiqjfjhocsisguo.supabase.co";
+const SUPABASE_URL = "https://oyguhhuumjzrcsrnbgzw.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6aHp1ZGlxamZqaG9jc2lzZ3VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyMzcxNDcsImV4cCI6MjA3OTgxMzE0N30.ZUo0XWKdjdoB6mjy0LBULIJ-X3SlHP9kVT_XWGTfl-g";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95Z3VoaHV1bWp6cmNzcm5iZ3p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzNzY4NDEsImV4cCI6MjA4Mzk1Mjg0MX0.VN-KSUtegrx4-kJZP-5UjpMaooGyx58w5yypzzODtkc";
 
 export const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
