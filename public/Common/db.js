51e5623e428a104c811a146a85b7761b5b2374d5
Yasmin <yasmiineri@gmail.com>
2026-02-05 12:17:34 +0100

    mantener sesion

--- public/Common/db.js
+++ public/Common/db.js
@@ -2,7 +2,14 @@ const SUPABASE_URL = "https://oyguhhuumjzrcsrnbgzw.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95Z3VoaHV1bWp6cmNzcm5iZ3p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzNzY4NDEsImV4cCI6MjA4Mzk1Mjg0MX0.VN-KSUtegrx4-kJZP-5UjpMaooGyx58w5yypzzODtkc";
 
-export const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storageKey: "sb-ultr-auth",
+  },
+});
 
 export function getLocalSession() {
   const raw = localStorage.getItem("ultraUser");
