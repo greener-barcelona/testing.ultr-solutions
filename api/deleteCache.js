8796e0bd31f75d534b781120a1ef9c3256906e5f
David Román Hernández <150918407+davidroman00@users.noreply.github.com>
2026-02-20 11:39:04 +0100

    fix

--- api/deleteCache.js
+++ api/deleteCache.js
@@ -1,6 +1,6 @@
 import { gemini } from "../lib/geminiAuth.js";
 
-export async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método no permitido" });
   }
